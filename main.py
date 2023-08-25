15993435755a32ba3ab05e36b411a2b1c265b958
Danial Hamedi <danial@Danials-Mac-Studio.local>
2023-08-25 23:16:12 +0330

    i will check if the latitude and longitude is above iran contury or not

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import math
+import pdb
 import iss_position
 
-(iss_position.iss_position())
+
+latitude, longitude, day, month, year, hour, minutes =\
+        iss_position.iss_position()
+print(round(float(latitude)))
+if 40 <= round(float(latitude)) <= 60 and 25 <= round(float(longitude)) <= 40:
+    print("you can see")
+else:
+    print("ridi")
