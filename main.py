2bd3f976764a288f9b0257412c34b0d2c903cb1e
Danial Hamedi <danial@Danials-Mac-Studio.local>
2023-08-26 00:33:57 +0330

    add all function and also works with zappier

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-import pdb
+import requests
 import iss_position
 
 
@@ -8,5 +7,13 @@ latitude, longitude, day, month, year, hour, minutes =\
 print(round(float(latitude)))
 if 40 <= round(float(latitude)) <= 60 and 25 <= round(float(longitude)) <= 40:
     print("you can see")
+    data = {"result": "you can see"}
+    requests.post("https://hooks.zapier.com/hooks/catch/16304266/3516k6i/",
+                  data=data,
+                  timeout=10)
 else:
-    print("ridi")
+    print("you cant see")
+    data = {"result": "you can not see"}
+    requests.post("https://hooks.zapier.com/hooks/catch/16304266/3516k6i/",
+                  data=data,
+                  timeout=10)
