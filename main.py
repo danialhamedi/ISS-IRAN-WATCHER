a8c703a850aaf7e377fef481f83799d3b81a6f7b
Danial Hamedi <danial@Danials-Mac-Studio.local>
2023-08-26 03:48:03 +0330

    this version works and sends a meaning ful meessage to users telegram

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ latitude, longitude, day, month, year, hour, minutes =\
 print(round(float(latitude)))
 if 40 <= round(float(latitude)) <= 60 and 25 <= round(float(longitude)) <= 40:
     print("you can see")
-    data = {"result": "you can see"}
+    data = {"result": "Guess who's photobombing the sky? ISS! Look up and smile, you're on cosmic camera! 📸🚀"}
     requests.post("https://hooks.zapier.com/hooks/catch/16304266/3516k6i/",
                   data=data,
                   timeout=10)
 else:
     print("you cant see")
-    data = {"result": "you can not see"}
-    requests.post("https://hooks.zapier.com/hooks/catch/16304266/3516k6i/",
-                  data=data,
-                  timeout=10)
+    #data = {"result": "you can not see"}
+    #requests.post("https://hooks.zapier.com/hooks/catch/16304266/3516k6i/",
+    #              data=data,
+    #              timeout=10)
